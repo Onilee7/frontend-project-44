9cf5605a1eec02110670e6f0871acd20c348fff5
Onilee7 <annabardasheva@gmail.com>
2023-03-07 06:52:20 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
-import runGameWithCounter from '../src/allGames.js';
-runGameWithCounter('brain-progression');
+import runGameWithCounter from "../src/allGames.js";
+
+runGameWithCounter("brain-progression");
