fd072b31bb15d3adf53d71bc996530d29770f9fe
Onilee7 <annabardasheva@gmail.com>
2023-03-07 09:52:09 +0300

    Update allGames.js

--- src/allGames.js
+++ src/allGames.js
@@ -23,22 +23,22 @@ function getUsersName() {
 
 // Правила игры
 const rulesOfGame = (nameGame) => {
-  switch (nameGame) {
- case "brain-even":
-  console.log('Answer \"yes\" if the number is even, otherwise answer \"no\".');
-break;
-  case "brain-calc":
-  console.log('What is the result of the expression?');
-break;
-case "brain-gcd":
-  console.log('Find the greatest common divisor of given numbers.');
-break;	
-case "brain-progression":
-  console.log('What number is missing in the progression?');
-break;
-case "brain-prime":
-  console.log('Answer \"yes\" if given number is prime. Otherwise answer \"no\".');
-break;
+switch (nameGame) {
+    case "brain-even":
+      console.log('Answer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case "brain-calc":
+      console.log('What is the result of the expression?');
+      break;
+    case "brain-gcd":
+      console.log('Find the greatest common divisor of given numbers.');
+      break;	
+    case "brain-progression":
+      console.log('What number is missing in the progression?');
+      break;
+    case "brain-prime":
+      console.log('Answer "yes" if given number is prime. Otherwise answer "no.');
+      break;
 	}
 };
 
