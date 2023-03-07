ff0bbc6734d60664a5be8147abd7e41617549efa
Onilee7 <annabardasheva@gmail.com>
2023-03-07 06:52:12 +0300

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line quotes
-import runGameWithCounter from '../src/allGames.js';
-runGameWithCounter('brain-gcd');
+import runGameWithCounter from "../src/allGames.js";
+
+runGameWithCounter("brain-gcd");
