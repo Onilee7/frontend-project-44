9aa8ece86be0439400b85fcfa747b89ad83111f0
Onilee7 <annabardasheva@gmail.com>
2023-03-07 10:55:56 +0300

    Update allGames.js

--- src/allGames.js
+++ src/allGames.js
@@ -51,57 +51,57 @@ function getRandom(min, max) {
 
 // Рандомный математический знак
 const getRandomMathSign = () => {
-	const arr = ['+', '-', '*'];
-	const i = Math.floor(Math.random() * arr.length);
-	const operator = arr[i];
-	return operator;
+  const arr = ['+', '-', '*'];
+  const i = Math.floor(Math.random() * arr.length);
+  const operator = arr[i];
+  return operator;
 };
 
 // Доп функция в игре brain-progression
 const progression = () => {
-	stepProgress = getRandom(2, 10);
-	const hideOfIndexNumber = getRandom(1, 10);
-	let arr = [];
-	const endProgression = firstRandomNumber + stepProgress * 10;
-	for (
-		let i = firstRandomNumber;
-		i < endProgression;
-		i += stepProgress
-	) {
-		arr.push(i);
-	}
-	ProgressionNumber = arr.splice(hideOfIndexNumber, 1, "..");
-	arr = arr.join(" ");
-	return arr;
+  stepProgress = getRandom(2, 10);
+  const hideOfIndexNumber = getRandom(1, 10);
+  let arr = [];
+  const endProgression = firstRandomNumber + stepProgress * 10;
+  for (
+    let i = firstRandomNumber;
+    i < endProgression;
+    i += stepProgress
+  ) {
+  arr.push(i);
+  }
+  ProgressionNumber = arr.splice(hideOfIndexNumber, 1, "..");
+  arr = arr.join(" ");
+  return arr;
 };
 
 // Вопрос игроку
 const question = (nameGame) => {
-	firstRandomNumber = getRandom(2, 100);
-	secondRandomNumber = getRandom(1, 100);
-	sign = getRandomMathSign();
-	let questionResult;
-	switch (nameGame) {
-  case 'brain-even':
+  firstRandomNumber = getRandom(2, 100);
+  secondRandomNumber = getRandom(1, 100);
+  sign = getRandomMathSign();
+  let questionResult;
+  switch (nameGame) {
+    case 'brain-even':
       questionResult = console.log(`${'Question:'} ${firstRandomNumber}`);
       break;
-  case 'brain-calc':
+    case 'brain-calc':
       questionResult = console.log(`${'Question:'} ${firstRandomNumber} ${sign} ${secondRandomNumber}`);
       break;
-  case 'brain-gcd':
+    case 'brain-gcd':
       questionResult = console.log(`${'Question:'} ${firstRandomNumber} ${secondRandomNumber}`);
       break;
-  case 'brain-progression':
+    case 'brain-progression':
       questionResult = console.log(`${'Question:'} ${progression(firstRandomNumber, stepProgress)}`);
       break;
-  case 'brain-prime':
+    case 'brain-prime':
       questionResult = console.log(`${'Question:'} ${firstRandomNumber}`);
       break;
-  default:
+    default:
       console.log('Sorry, something wrong');
       break;
-}
-return questionResult;
+  }
+  return questionResult;
 };
 
 // Ответ от пользователя
@@ -109,84 +109,81 @@ const getUsersAnswer = () => readlineSync.question('Your answer: ');
 
 // Расчет правильно ответа brain-even
 const brainEvenCorrectAnswer = (a) => {
-	if (a % 2 === 0) {
-		CorrectAnswer = "yes";
-	} else if (a % 2 !== 0) {
-		CorrectAnswer = "no";
-	}
-	return CorrectAnswer;
+  if (a % 2 === 0) {
+    CorrectAnswer = 'yes';
+  } else if (a % 2 !== 0) {
+    CorrectAnswer = 'no';
+  }
+  return CorrectAnswer;
 };
 
 // Правельный ответ brain-calc
 const brainCalcCorrectAnswer = (a, b) => {
-	if (sign === "+") {
-		CorrectAnswer = a + b;
-	} else if (sign === "-") {
-		CorrectAnswer = a - b;
-	} else {
-		CorrectAnswer = a * b;
-	}
-	return CorrectAnswer;
+  if (sign === '+') {
+    CorrectAnswer = a + b;
+  } else if (sign === '-') {
+    CorrectAnswer = a - b;
+  } else {
+  CorrectAnswer = a * b;
+  }
+  return CorrectAnswer;
 };
 
 // Расчет правильно ответа  brain-gcd
 
 const brainGcdCorrectAnswer = (a, b) => {
-	if (!b) {
-		return a;
-	}
-	return brainGcdCorrectAnswer(b, a % b);
+  if (!b) {
+    return a;
+  }
+    return brainGcdCorrectAnswer(b, a % b);
 };
 
 // Расчет правильно ответа  brain-prime
 const brainPrimeCorrectAnswer = (a) => {
-	for (let i = 2; i < a; i += 1) {
-		if (a % i === 0) {
-			return "no";
-		}
-	}
-	return "yes";
+  for (let i = 2; i < a; i += 1) {
+    if (a % i === 0) {
+       return 'no';
+    }
+  }
+  return 'yes';
 };
-
 
 // Правельный ответ в зависимости от игры
 const correctAnswer = (nameGame) => {
 	switch (nameGame) {
-	case "brain-even":
-		brainEvenCorrectAnswer(firstRandomNumber);
-		break;
-	case "brain-calc":
-		brainCalcCorrectAnswer(firstRandomNumber, secondRandomNumber);
-		break;
-	case "brain-gcd":
-		CorrectAnswer = brainGcdCorrectAnswer(firstRandomNumber, secondRandomNumber);
-		break;
-	case "brain-progression":
-		CorrectAnswer = ProgressionNumber;
-		break;
-	case "brain-prime":
-		CorrectAnswer = brainPrimeCorrectAnswer(firstRandomNumber);
-		break;
-	default:
-		console.log("Sorry, something wrong");
-		break;
-	}
-	return CorrectAnswer.toString();
+  case 'brain-even':
+    brainEvenCorrectAnswer(firstRandomNumber);
+      break;
+      case 'brain-calc':
+    brainCalcCorrectAnswer(firstRandomNumber, secondRandomNumber);
+      break;
+	case 'brain-gcd':
+    CorrectAnswer = brainGcdCorrectAnswer(firstRandomNumber, secondRandomNumber);
+      break;
+	case 'brain-progression':
+    CorrectAnswer = ProgressionNumber;
+      break;
+	case 'brain-prime':
+    CorrectAnswer = brainPrimeCorrectAnswer(firstRandomNumber);
+      break;
+      default:
+    console.log('Sorry, something wrong');
+      break;
+      }
+  return CorrectAnswer.toString();
 };
-
 
 const textOfcorrectAnswer = () => {
-	console.log("Correct!");
+  console.log('Correct!');
 };
 
-
 const compareOfAnswer = (nameGame) => {
-	const userAnswer = getUsersAnswer();
-	const answer = correctAnswer(nameGame);
+  const userAnswer = getUsersAnswer();
+    const answer = correctAnswer(nameGame);
 	if (answer === userAnswer) {
 		textOfcorrectAnswer();
 	} else {
-		console.log(`${userAnswer} ${"is wrong answer ;(. Correct answer was"} ${answer}.\n${"Let's try again,"} ${getUsersName()}!`);
+		console.log(`${userAnswer} ${'is wrong answer ;(. Correct answer was'} ${answer}.\n${"Let's try again,"} ${getUsersName()}!`);
 		GameOver = "true";
 	}
 };
@@ -203,7 +200,7 @@ const runGameWithCounter = (nameGame) => {
 		i += 1;
 	}
 	if (i === 3 && GameOver !== "true") {
-		console.log(`${"Congratulations,"} ${getUsersName()}!`);
+		console.log(`${'Congratulations,'} ${getUsersName()}!`);
 	}
 };
 
